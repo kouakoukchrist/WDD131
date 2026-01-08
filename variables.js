4512e93126a75c1b38b7b14c5a67138bbe9849bd
yourusername <kouakouchrist24@gmail.com>
2026-01-07 18:44:57 -0700

    Changes have been added

---
+++ variables.js
@@ -0,0 +1,29 @@
+
+const PI = 3.14;
+let radius = 3;
+
+let  area = radius* radius * PI;
+
+console.log(area);
+
+// type Coersion
+const one = 1;
+const two = 2;
+let result = one * two ;
+console.log(result);
+
+result = one + two;
+
+console.log(result);
+
+
+let course = "CSE131"; //global scope
+
+if (true) {
+    let student = "Christ KOUAKOU";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
