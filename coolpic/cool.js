c7db479b29cd24065987970dea0f63b2340a1a0e
yourusername <kouakouchrist24@gmail.com>
2026-01-24 17:16:56 -0700

    week03

---
+++ coolpic/cool.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const navLinks = document.getElementById("navLinks");
+
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+});
