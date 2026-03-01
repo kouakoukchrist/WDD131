b42563a27046a3c2fa7109734bdc3a7236238c5a
yourusername <kouakouchrist24@gmail.com>
2026-02-28 18:19:35 -0700

    updated

--- prove-08/prove.js
+++ prove-08/prove.js
@@ -1,37 +1,31 @@
-
-
 const Character = {
-name : 'Snortleblat',
-class : 'Swamp Beast Diploma',
-level: 8,
-health: 100,
+    name: 'Snortleblat',
+    class: 'Swamp Beast Diploma',
+    level: 8,
+    health: 100,
+
+    attacked: function() {
+        if (this.health <= 0) { return; }
+        this.health -= 20;
+        healthEl.textContent = 'Health: ' + this.health;
+        if (this.health <= 0) {
+            alert('Character Died');
+        } else if (this.health <= 20) {
+            alert('Character is almost dead');
+        }
+    },
+
+    levelUp: function() {
+        if (this.health <= 0) { return; }
+        this.level += 1;
+        levelEl.textContent = 'Level: ' + this.level;
+    }
 };
 
-
-const levelEL = document.querySelector('#characterLevel')
-const healthEl = document.querySelector('#characterHealth')
-const levelUpEL = document.querySelector('#characterLevelUp')
-const attackedEl = document.querySelector('#characterAttacked')
-
-
 attackedEl.addEventListener("click", () => {
-    if (Character.health <= 0) { return;  }
-    Character.health -= 20;
-    healthEl.textContent = 'Health: ' + Character.health;
-    if (Character.health <= 0) {
-    alert('Character Died');
-} else if (Character.health <= 20) {
-    alert('Character is almost dead');
-}
-   
+    Character.attacked();
 });
 
-
-levelUpEL.addEventListener("click",()=>{
-    if (Character.health <= 0) { return;  }
-Character.level += 1
-levelEL.textContent = 'Level: ' + Character.level;
-
-
+levelUpEl.addEventListener("click", () => {
+    Character.levelUp();
 });
-
