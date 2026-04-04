ffb3377612ff0f0ea2950de581a59d7295a74355
yourusername <kouakouchrist24@gmail.com>
2026-04-04 03:35:50 -0600

    up

--- finals/AboutUs.js
+++
@@ -1,9 +0,0 @@
-let searchBanner = document.getElementById('bannersearch');
-
-if (searchBanner) {
-  searchBanner.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-         window.location.href = "inventory.html";
-      }
-  });
-}
